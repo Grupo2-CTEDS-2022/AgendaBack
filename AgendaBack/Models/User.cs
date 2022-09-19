ba6e70fba75236b533e0a9d53e25e9ca06ef22c3
BnyioM <brunomaia.duarte@gmail.com>
2022-09-19 20:56:16 -0300

    Delete Account

--- AgendaBack/Models/User.cs
+++ AgendaBack/Models/User.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace AgendaBack.Models
 {
-    internal class User
+    public class User
     {
         public int Id { get; set; }
-        
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
     }
 }
