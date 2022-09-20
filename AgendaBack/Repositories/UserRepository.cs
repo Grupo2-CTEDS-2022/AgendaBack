e6d37f6ac5174850e6456b6673764ecec73ac6b9
BnyioM <brunomaia.duarte@gmail.com>
2022-09-19 21:41:48 -0300

    Generate Token

---
+++ AgendaBack/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using AgendaBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaBack.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string Name, string Password)
+        {
+            var users = new List<User>();
+
+            return users.Where(x => x.Name.ToLower() == Name.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
